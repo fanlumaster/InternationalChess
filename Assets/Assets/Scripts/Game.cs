637dc6c7f1ad651e6ea4f2e3b3880b105f3809a4
fanlumaster <1730976608@qq.com>
2021-03-03 23:03:21 +0800

    first commit

---
+++ Assets/Assets/Scripts/Game.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public GameObject chesspiece;
+
+    // Positions and team for each chesspiece
+    private GameObject[,] positions = new GameObject[8, 8];
+    private GameObject[] playerBlack = new GameObject[16];
+    private GameObject[] playerWhite = new GameObject[16];
+
+    private string currentPlayer = "white";
+
+    private bool gameOver = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 创造相应的 GameObject
+        playerWhite = new GameObject[]
+        {
+            Create("white_rook", 0, 0), Create("white_knight", 1, 0),
+            Create("white_bishop", 2, 0), Create("white_queen", 3, 0),
+            Create("white_king", 4, 0), Create("white_bishop", 5, 0),
+            Create("white_knight", 6, 0), Create("white_rook", 7, 0),
+            Create("white_pawn", 0, 1), Create("white_pawn", 1, 1),
+            Create("white_pawn", 2, 1), Create("white_pawn", 3, 1),
+            Create("white_pawn", 4, 1), Create("white_pawn", 5, 1),
+            Create("white_pawn", 6, 1), Create("white_pawn", 7, 1)
+        };
+        playerBlack = new GameObject[]
+        {
+            Create("black_rook", 0, 7), Create("black_knight", 1, 7),
+            Create("black_bishop", 2, 7), Create("black_queen", 3, 7),
+            Create("black_king", 4, 7), Create("black_bishop", 5, 7),
+            Create("black_knight", 6, 7), Create("black_rook", 7, 7),
+            Create("black_pawn", 0, 6), Create("black_pawn", 1, 6),
+            Create("black_pawn", 2, 6), Create("black_pawn", 3, 6),
+            Create("black_pawn", 4, 6), Create("black_pawn", 5, 6),
+            Create("black_pawn", 6, 6), Create("black_pawn", 7, 6)
+        };
+
+        // Set all piece positions on the position board
+        for (int i = 0; i < playerBlack.Length; i++)
+        {
+            SetPosition(playerBlack[i]);
+            SetPosition(playerWhite[i]);
+        }
+
+    }
+
+    // 创建带有（放置）坐标的 GameObject
+    public GameObject Create(string name, int x, int y)
+    {
+        GameObject obj = Instantiate(chesspiece, new Vector3(0, 0, -1), Quaternion.identity);
+        Chessman cm = obj.GetComponent<Chessman>();
+        cm.name = name;
+        cm.SetXBoard(x);
+        cm.SetYBoard(y);
+        cm.Activate();
+        return obj;
+    }
+
+    // 将传入的 GameObject 放置在相应的位置
+    public void SetPosition(GameObject obj)
+    {
+        Chessman cm = obj.GetComponent<Chessman>();
+
+        positions[cm.GetXBoard(), cm.GetYBoard()] = obj;
+    }
+
+    // 将指定位置清空
+    public void SetPositionEmpty(int x, int y)
+    {
+        positions[x, y] = null;
+    }
+
+    // 获取指定坐标处的 GameObject
+    public GameObject GetPosition(int x, int y)
+    {
+        return positions[x, y];
+    }
+
+    // 判断坐标是否在棋盘上
+    public bool PositionOnBoard(int x, int y)
+    {
+        if (x < 0 || y < 0 || x >= positions.GetLength(0) || y >= positions.GetLength(1))
+            return false;
+        return true;
+    }
+
+    public string GetCurrentPlayer()
+    {
+        return currentPlayer;
+    }    
+
+    public bool IsGameOver()
+    {
+        return gameOver;
+    }
+
+    public void NextTurn()
+    {
+        if (currentPlayer == "white")
+        {
+            currentPlayer = "black";
+        } 
+        else
+        {
+            currentPlayer = "white";
+        }
+    }
+
+    public void Update()
+    {
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            gameOver = false;
+
+            SceneManager.LoadScene("Game");
+        }
+    }
+    
+    // 游戏获胜处理
+    public void Winner(string playerWinner)
+    {
+        gameOver = true;
+
+        GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().enabled = true; // 显示 Text Component
+        GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().text = playerWinner + " is the winner"; // 打印获胜者的信息
+
+        GameObject.FindGameObjectWithTag("RestartText").GetComponent<Text>().enabled = true;
+    }
+}
---
+++ Assets/Assets/Scripts/MovePlate.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlate : MonoBehaviour
+{
+    public GameObject controller;
+
+    GameObject reference = null;
+
+    // Board positions, not world positions
+    int matrixX;
+    int matrixY;
+
+    // false: movement, true: attacking
+    public bool attack = false;
+
+    public void Start()
+    {
+        if (attack)
+        {
+            // Change to red
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+        }
+    }
+
+    // 鼠标抬起时触发
+    public void OnMouseUp()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+
+        if (attack)
+        {
+            // 如果 MovePlate Sprite 和 Chessman Sprite 是在同一个坐标下，即象棋棋子是处于有红色的被瞄准的状态，那么，MovePlate 是相当于覆盖在 Chessman 上的，并且，这个鼠标监听只能监听到处于最上层的 Sprite，在这里就是 MovePlate
+            GameObject cp = controller.GetComponent<Game>().GetPosition(matrixX, matrixY);
+
+            if (cp.name == "white_king") controller.GetComponent<Game>().Winner("black");
+            if (cp.name == "black_king") controller.GetComponent<Game>().Winner("white");
+
+            Destroy(cp);
+        }
+
+        controller.GetComponent<Game>().SetPositionEmpty(reference.GetComponent<Chessman>().GetXBoard(),
+            reference.GetComponent<Chessman>().GetYBoard());
+
+        reference.GetComponent<Chessman>().SetXBoard(matrixX);
+        reference.GetComponent<Chessman>().SetYBoard(matrixY);
+        reference.GetComponent<Chessman>().SetCoords();
+
+        controller.GetComponent<Game>().SetPosition(reference);
+
+        controller.GetComponent<Game>().NextTurn();
+
+        reference.GetComponent<Chessman>().DestroyMovePlates();
+    }
+
+    public void SetCoords(int x, int y)
+    {
+        matrixX = x;
+        matrixY = y;
+    }
+
+    public void SetReference(GameObject obj)
+    {
+        reference = obj;
+    }
+
+    public GameObject GetReference()
+    {
+        return reference;
+    }
+}
